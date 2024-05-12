7ff77d82abec6e9c9009fffbcf6dc1dc1f8f8899
Nishat Firoj <nishat.firoj@gmail.com>
2024-05-12 12:47:49 -0400

    separate ios and android files and implement maps in the process

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { StyleSheet, Text, View, ScrollView } from "react-native"
 import { useAllBars } from "./services/endpoints"
+import { Map } from "./src/components"
 
 export default function App() {
   const response = useAllBars()
   return (
     <View style={styles.container}>
+      <Map />
       <ScrollView>
         {response.map((value: any, index: number) => (
           <Text key={index}>{value.barName}</Text>
